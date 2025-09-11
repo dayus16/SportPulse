3be4954c862ee9c372138f4ff4e16a0b0cd37d87
DAYO ODOJE <dayodoje@gmail.com>
2025-09-11 09:49:12 -0400

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -206,7 +206,7 @@ const Home = ({ input }) => {
 
           <div className="bg-gray-800 py-2 px-4 text-white font-semibold text-lg rounded mt-10 hover:bg-[#e93314] hover:text-white transition-colors duration-300">
             <Link className="flex items-center justify-center gap-2" to="/blog">
-              Go To Blog Page
+              Read all News
               <FaLongArrowAltRight />
             </Link>
           </div>
